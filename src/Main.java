56bf405cb67d03c406956b3492b1ce14de754594
armandoCodigo <88814046+Armandoc26@users.noreply.github.com>
2022-10-02 20:40:56 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,59 @@
+import java.text.Format;
+
 public class Main {
     public static void main(String[] args) {
+                    //Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+            int numeroIf = 0;
+                if (numeroIf < 0) {
+                    System.out.println("Negativo");
+                }
+                else if (numeroIf > 0) {
+                    System.out.println("positivo");
+                }
+                else {
+                    System.out.println("0");
+                }
 
-        coche miCoche = new coche();
-        miCoche.incrementarPuertasDelCoche();
-        System.out.println(miCoche.puertasDelCohe);
+                     //Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3...
+                int numeroWhile = 0;
 
-        int resultado;
-        resultado = suma(20, 30, 50);
-        System.out.println(resultado);
+                    while (numeroWhile < 3) {
+                        System.out.println(numeroWhile);
+                        numeroWhile = numeroWhile + 1;
+                    }
 
-    }
+                    //bucle Do While
+                     do {
+                         System.out.println(numeroWhile);
+                         numeroWhile = numeroWhile + 1;
+                    } while (numeroWhile < 3);
 
-    public static int suma(int a, int b, int c) {
-        return a + b + c;
-    }
-    public static class coche {
-        public int puertasDelCohe = 4;
+                    // bucle For
+             for (int numeroFor = 0; numeroFor <= 3; numeroFor = numeroFor + 1) {
+                 System.out.println(numeroFor);
+             }
 
-        public void incrementarPuertasDelCoche() {
-            this.puertasDelCohe++;
-        }
+                    // Switch Case
+
+            String estacion = "otoño";
+
+             switch (estacion) {
+                 case "verano":
+                     System.out.println("Es Verano");
+                     break;
+                 case "primavera":
+                     System.out.println("Es Primavera");
+                     break;
+                 case "otoño":
+                     System.out.println("Es Otoño");
+                     break;
+                 case "invierno":
+                     System.out.println("Es Invierno");
+                     break;
+                 default:
+                     System.out.println("no es una estacion");
+
+             }
 
     }
 }
